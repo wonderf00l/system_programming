fa28a1402da6555c74ad953ed5cb8b4fd384bbec
22069313 <22069313@sigma.sbrf.ru@CAB-WSD-0002831>
2024-11-23 14:47:36 +0300

    vault backup: 2024-11-23 14:47:36

--- code-notes/pipes/main.c
+++ code-notes/pipes/main.c
@@ -2,8 +2,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main()
+int main(int argc, const char **argv)
 {
     int to_parent[2];
     int to_child[2];
@@ -24,6 +25,41 @@ int main()
     write(to_child[1], "hello1", sizeof("hello"));
     read(to_parent[0], buf, sizeof(buf));
     printf("%d: read %s\n", (int)getpid(), buf);
+    wait(NULL);
+
+    int p = pipes1(argc, argv); // внутри создается новый процесс: стек вызовов сохранится, дочерний процесс вернет 123 из pipe1 и напечатает в свой stdout дескриптор, т.к. они дублируются - видим вывод 2-ух процессов в одну и ту же консоль
+    printf("pipes1 res: %d\n", p);
+
+    return 0;
+}
+
+int pipes1(int argc, const char **argv) // будем блокироваться, если не закроем duplicated дескрипторы
+{
+    int channel[2];
+    int need_close = argc > 1 && strcmp(argv[1], "close") == 0;
+    pipe(channel);
+    if (fork() == 0)
+    {
+        char buf[16];
+        printf("child: started\n");
+        if (need_close)
+        {
+            printf("child: close output channel\n");
+            close(channel[1]);
+        }
+        while (read(channel[0], buf, sizeof(buf)) > 0)
+            printf("child: read %s\n", buf);
+        printf("child: EOF\n");
+        return 123;
+    }
+    write(channel[1], "100", 3);
+    printf("parent: written 100\n");
+    if (need_close)
+    {
+        printf("parent: close output channel\n");
+        close(channel[1]);
+    }
+    printf("parent: waiting for child termination ...\n");
     wait(NULL);
     return 0;
 }
