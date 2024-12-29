4eeb1de017c3a40780bd1a7852ef986004004775
wonderf00l <105116952+wonderf00l@users.noreply.github.com>
2024-12-29 16:00:09 +0300

    Update main.c

--- code-notes/pipes/main.c
+++ code-notes/pipes/main.c
@@ -64,3 +64,23 @@ int pipes1(int argc, const char **argv) // существовать, т.к. ос
     wait(NULL);
     return 0;
 }
+
+
+int startAnotherProgramWithStdinFdLinkedToReadingEndOfThePipe()
+{
+	int to_child[2];
+	pipe(to_child);
+	char buf[16];
+	dup2(to_child[0], 0);
+	if (fork() == 0) {
+		close(to_child[1]);
+		int n;
+		scanf("%d", &n);
+		printf("%d: read %d\n", (int) getpid(), n);
+		return 0;
+	}
+	close(to_child[0]);
+	write(to_child[1], "100", sizeof("100"));
+	wait(NULL);
+	return 0;
+}
